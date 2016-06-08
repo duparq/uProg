5bdbcc16d41505e8957f30b471aa2516e09e9019
Christophe Duparquet <duparq@free.fr>
2016-06-08 22:41:33 +0200

    Derivated a new block for set-variable that uses an arrow.

---
+++ blocks/variable_set.js
@@ -0,0 +1,54 @@
+
+/* Copyright (c) 2016 Christophe Duparquet.
+ * http://github.com/duparq/ublockly
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/*  A special version of Blockly's 'set variable' block
+ */
+
+'use strict';
+
+
+Blockly.Blocks['variables_set'] = {
+  /**
+   * Block for variable setter.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 %2%3",
+      "args0": [
+        {
+          "type": "field_variable",
+          "name": "VAR",
+          "variable": Blockly.Msg.VARIABLES_DEFAULT_NAME
+        },
+	{
+	  "type": "field_image",
+	  "src": "file:///home/indy/dev/ublockly/icons/variable_set.svg",
+	  "width": 20,
+	  "height": 15,
+	  "alt": "<--"
+	},
+        {
+          "type": "input_value",
+          "name": "VALUE"
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": Blockly.Blocks.variables.HUE,
+      "tooltip": Blockly.Msg.VARIABLES_SET_TOOLTIP,
+      "helpUrl": Blockly.Msg.VARIABLES_SET_HELPURL
+    });
+    this.contextMenuMsg_ = Blockly.Msg.VARIABLES_SET_CREATE_GET;
+  },
+  contextMenuType_: 'variables_get',
+  customContextMenu: Blockly.Blocks['variables_get'].customContextMenu
+};
